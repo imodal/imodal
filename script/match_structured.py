31fcda7ad5edca1ed091d5dda1270f8a089905c6
Barbara Gris <gris.maths@gmail.com>
2018-12-19 17:07:25 +0100

    new example for matching

--- script/match_structured.py
+++ script/match_structured.py
@@ -53,34 +53,63 @@ nlxt[:,0]  = scale*(nlxt[:,0]-np.mean(nlxt[:,0]))
 xst = nlxt[nlxt[:,2]==2,0:2]
 
 
-#
-##%% autre source
-#with open('../data/basi2temp.pkl', 'rb') as f:
-#    img, lx = pickle.load(f)
-#    
+
+
+
+#%% autre source
+
+height = 38.
+heightt = 209.
+
+
+with open('../data/basi2temp.pkl', 'rb') as f:
+    img, lx = pickle.load(f)
+    
 #nlx = np.asarray(lx).astype(np.float32)
 #(lmin, lmax) = (np.min(nlx[:,1]),np.max(nlx[:,1]))
-#scale = 180./(lmax-lmin)
+#scale = 38./(lmax-lmin)
 #
-#nlx[:,1]  = 180.0 - scale*(nlx[:,1]-lmin) 
+#nlx[:,1]  =  - scale*(nlx[:,1]-lmin) 
 #nlx[:,0]  = scale*(nlx[:,0]-np.mean(nlx[:,0]))           
-#
-#x0 = nlx[nlx[:,2]==0,0:2]
-#x1 = nlx[nlx[:,2]==1,0:2]
-#xs = nlx[nlx[:,2]==2,0:2]
-#
-##%% autre target
-#with open('../data/basi2target.pkl', 'rb') as f:
-#    imgt, lxt = pickle.load(f)
-#
+
+Dx = 0
+Dy = 0
+
+nlx = np.asarray(lx).astype(np.float64)
+nlx[:,1] = - nlx[:,1]
+(lmin, lmax) = (np.min(nlx[:,1]),np.max(nlx[:,1]))
+
+scale = height/(lmax-lmin)
+nlx[:,0]  = scale*(nlx[:,0]-np.mean(nlx[:,0])) + Dx
+nlx[:,1]  = scale*(nlx[:,1]-lmin) + Dy
+
+
+x0 = nlx[nlx[:,2]==0,0:2]
+x1 = nlx[nlx[:,2]==1,0:2]
+xs = nlx[nlx[:,2]==2,0:2]
+
+#%% autre target
+with open('../data/basi2target.pkl', 'rb') as f:
+    imgt, lxt = pickle.load(f)
+
 #nlxt = np.asarray(lxt).astype(np.float32)
 #(lmin, lmax) = (np.min(nlxt[:,1]),np.max(nlxt[:,1]))
-#scale = 495./(lmax-lmin)
+#scale = 209./(lmax-lmin)
 #
-#nlxt[:,1]  = 90.0 - scale*(nlxt[:,1]-lmin) + 400
+#nlxt[:,1]  = 90.0 - scale*(nlxt[:,1]-lmin) #+ 400
 #nlxt[:,0]  = scale*(nlxt[:,0]-np.mean(nlxt[:,0]))            
-#
-#xst = nlxt[nlxt[:,2]==2,0:2]
+
+nlxt = np.asarray(lxt).astype(np.float64)
+nlxt[:,1] = -nlxt[:,1]
+(lmint, lmaxt) = (np.min(nlxt[:,1]),np.max(nlxt[:,1]))
+scale = heightt/(lmaxt-lmint)
+
+
+nlxt[:,1]  = scale*(nlxt[:,1] - lmint)
+nlxt[:,0]  = scale*(nlxt[:,0]-np.mean(nlxt[:,0]))
+
+
+xst = nlxt[nlxt[:,2]==2,0:2]
 
 
 #%% parameter for module of order 1
@@ -117,6 +146,33 @@ C[:,0,0] = 1.*C[:,1,0]
 #C[:,0,0] = 1.*C[:,1,1]
 ##
 
+#%% new shapes
+
+th = 0*np.pi
+th = th*np.ones(x1.shape[0])
+R = np.asarray([rot.my_R(cth) for cth in th])
+for  i in range(x1.shape[0]):
+    R[i] = rot.my_R(th[i])
+
+C = np.zeros((x1.shape[0],2,1))
+
+K = 10
+
+height = 38.
+L = height
+
+a = 1/L
+
+b = 3.
+Dy = 0.
+
+z = a*(x1[:,1]-Dy)
+
+C[:,1,0] = K*((1-b)*z**2+b*z)
+
+C[:,0,0] = 0.9*C[:,1,0]
+
+
 #%% plot C profile
 plt.figure()
 X = np.linspace(0,38,100)
@@ -136,21 +192,21 @@ plt.plot(X,Z, '-')
 #%%
 x00 = np.array([[0., 0.]])
 coeffs = [5., 0.01]
-sig0 = 5
-sig00 = 200
-sig1 = 30
+sig0 = 30
+sig00 = 500
+sig1 = 50
 nu = 0.001
 dim = 2
 Sil = defmod.SilentLandmark(xs.shape[0], dim)
 Model1 = defmod.ElasticOrder1(sig1, x1.shape[0], dim, coeffs[1], C, nu)
 Model01 = defmod.ElasticOrder1(sig0, x1.shape[0], dim, coeffs[1], C, nu)
-Model0 = defmod.ElasticOrderO(sig0, x0.shape[0], dim, coeffs[0])
-Model00 = defmod.ElasticOrderO(sig00, x00.shape[0], dim, 0.1)
+Model0 = defmod.ElasticOrderO(sig0, x0.shape[0], dim, coeffs[0], nu)
+Model00 = defmod.ElasticOrderO(sig00, x00.shape[0], dim, 0.1, nu)
 #%% 
 
-#Mod_el_init = comb_mod.CompoundModules([Sil, Model00, Model1])
+Mod_el_init = comb_mod.CompoundModules([Sil, Model00, Model0, Model1])
 
-Mod_el_init = comb_mod.CompoundModules([Sil, Model1])
+#Mod_el_init = comb_mod.CompoundModules([Sil, Model1])
 
 #%%
 p0 = np.zeros(x0.shape)
@@ -164,8 +220,8 @@ param_00 = (np.zeros([1, 2]), np.zeros([1, 2]))
 param_1 = ((x1, R), (p1, PR))
 
 #%%
-#param = [param_sil, param_0, param_1]
-param = [param_sil, param_1]
+param = [param_sil, param_00, param_0, param_1]
+#param = [param_sil, param_1]
 GD = Mod_el_init.GD.copy()
 Mod_el_init.GD.fill_cot_from_param(param)
 Mod_el = Mod_el_init.copy_full()
@@ -211,16 +267,16 @@ Mod_el_init_opti.GD.fill_cot_from_param(param)
 Mod_el_opti = Mod_el_init_opti.copy_full()
 P0 = opti.fill_Vector_from_GD(Mod_el_opti.GD)
 #%%
-lam_var = 10.
+lam_var = 15.
 sig_var = 30.
 N=5
 args = (Mod_el_opti, xst, lam_var, sig_var, N, 0.001)
 
-res = scipy.optimize.minimize(opti.fun, P0,
+res = scipy.optimize.minimize(opti.fun, P1,
     args = args,
     method='L-BFGS-B', jac=opti.jac, bounds=None, tol=None, callback=None,
     options={'disp': True, 'maxcor': 10, 'ftol': 1.e-09, 'gtol': 1e-03,
-    'eps': 1e-08, 'maxfun': 100, 'maxiter': 10, 'iprint': -1, 'maxls': 20})
+    'eps': 1e-08, 'maxfun': 100, 'maxiter': 5, 'iprint': -1, 'maxls': 20})
 #%%µ
 P1 = res['x']
 
@@ -241,13 +297,22 @@ opti.fill_Mod_from_Vector(P1, Mod_el_opti)
 ##plt.plot(xsf[:,0], xsf[:,1], '-g')
 
 #%% Plot with grid
-height = 400
-height = 40
-nx, ny = (11,21) # create a grid for visualisation purpose
+#height = 400
+#height = 40
+#nx, ny = (11,21) # create a grid for visualisation purpose
+#u = height/38.
+#Dx = 0.
+#Dy = 0.
+#(a,b,c,d) = (-10.*u + Dx, 10.*u + Dx, -3.*u + Dy, 40.*u + Dy)
+
+#new grid
+nx, ny = (5,11) # create a grid for visualisation purpose
 u = height/38.
-Dx = 0.
-Dy = 0.
 (a,b,c,d) = (-10.*u + Dx, 10.*u + Dx, -3.*u + Dy, 40.*u + Dy)
+
+
+
+
 [xx, xy] = np.meshgrid(np.linspace(a,b,nx), np.linspace(c,d,ny))
 (nx,ny) = xx.shape
 grid_points= np.asarray([xx.flatten(), xy.flatten()]).transpose()
@@ -296,13 +361,13 @@ for i in range(N+1):
     x00_i = Modlist_opti_tot[2*i].GD.Cot['0'][2][0]
     plt.plot(x00_i[:,0], x00_i[:,1], '.r')
     x00_i = Modlist_opti_tot[2*i].GD.Cot['0'][3][0]
-    plt.plot(x00_i[:,0], x00_i[:,1], 'xr', markersize=1)
+    plt.plot(x00_i[:,0], x00_i[:,1], 'xr')#, markersize=1)
     plt.axis('equal')
     #plt.axis([-20,50,-10,105])
-    plt.axis([-40,30,-70,45])
+    #plt.axis([-40,30,-70,45])
     #plt.axis([-10,10,-10,55])
     #plt.axis([-10,60,-10,70])
-    plt.axis('off')
+    #plt.axis('off')
     plt.savefig(path_res + name_ex + '_t_' + str(i) + '.pdf', format='pdf', bbox_inches = 'tight')
 
 
