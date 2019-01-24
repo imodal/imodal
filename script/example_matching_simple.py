b0c3f8d32f842a48678a9747774bf2f13195ce83
Barbara Gris <gris.maths@gmail.com>
2019-01-24 19:54:48 +0100

    Small example working

--- script/example_matching_simple.py
+++ script/example_matching_simple.py
@@ -82,6 +82,36 @@ nlxt[:,0]  = scale*(nlxt[:,0]-np.mean(nlxt[:,0]))
 
 
 xst = nlxt[nlxt[:,2]==2,0:2]
+#%%
+
+
+path_data = '../data/'
+with open(path_data + 'basi1b.pkl', 'rb') as f:
+    img, lx = pickle.load(f)
+
+nlx = np.asarray(lx).astype(np.float32)
+(lmin, lmax) = (np.min(nlx[:, 1]), np.max(nlx[:, 1]))
+scale = 38. / (lmax - lmin)
+
+nlx[:, 1] = 38.0 - scale * (nlx[:, 1] - lmin)
+nlx[:, 0] = scale * (nlx[:, 0] - np.mean(nlx[:, 0]))
+
+x0 = nlx[nlx[:, 2] == 0, 0:2]
+x1 = nlx[nlx[:, 2] == 1, 0:2]
+xs = nlx[nlx[:, 2] == 2, 0:2]
+
+# %% target
+with open(path_data + 'basi1t.pkl', 'rb') as f:
+    imgt, lxt = pickle.load(f)
+
+nlxt = np.asarray(lxt).astype(np.float32)
+(lmin, lmax) = (np.min(nlxt[:, 1]), np.max(nlxt[:, 1]))
+scale = 100. / (lmax - lmin)
+
+nlxt[:, 1] = 38.0 - scale * (nlxt[:, 1] - lmin)
+nlxt[:, 0] = scale * (nlxt[:, 0] - np.mean(nlxt[:, 0]))
+
+xst = nlxt[nlxt[:, 2] == 2, 0:2]
 
 
 #%% parameter for module of order 1
@@ -94,26 +124,21 @@ for  i in range(x1.shape[0]):
 C = np.zeros((x1.shape[0],2,1))
 
 K = 10
-
 height = 38.
+C = np.zeros((x1.shape[0],2,1))
+K = 10
+
 L = height
+a, b = -2/L**3, 3/L**2
+C[:,1,0] = K*(a*(L-x1[:,1]+Dy)**3 + b*(L-x1[:,1]+Dy)**2)
+C[:,0,0] = 1.*C[:,1,0]
 
-a = 1/L
-
-b = 3.
-Dy = 0.
-
-z = a*(x1[:,1]-Dy)
-
-C[:,1,0] = K*((1-b)*z**2+b*z)
-
-C[:,0,0] = 0.9*C[:,1,0]
 #%%
 x00 = np.array([[0., 0.]])
 coeffs = [1., 0.01]
-sig0 = 30
-sig00 = 500
-sig1 = 50
+sig0 = 20
+sig00 = 200
+sig1 = 30
 nu = 0.001
 dim = 2
 Sil = defmodsil.SilentLandmark(xs.shape[0], dim)
@@ -123,6 +148,8 @@ Model00 = defmod0.ElasticOrderO(sig00, x00.shape[0], dim, 0.1, nu)
 #%% 
 
 Mod_el_init = comb_mod.CompoundModules([Sil, Model00, Model0, Model1])
+
+#Mod_el_init = comb_mod.CompoundModules([Sil, Model00, Model1])
 
 
 #%%
@@ -139,6 +166,7 @@ param_1 = ((x1, R), (p1, PR))
 
 #%%
 param = [param_sil, param_00, param_0, param_1]
+#param = [param_sil, param_00, param_1]
 GD = Mod_el_init.GD.copy()
 
 #%%
@@ -155,8 +183,8 @@ Modlist = shoot.shooting_traj(Mod_el, N)
 Mod_el_opti = Mod_el_init.copy_full()
 P0 = opti.fill_Vector_from_GD(Mod_el_opti.GD)
 # %%
-lam_var = 15.
-sig_var = 10.
+lam_var = 10.
+sig_var = 30.
 N = 5
 args = (Mod_el_opti, xst, lam_var, sig_var, N, 0.001)
 
@@ -164,7 +192,7 @@ res = scipy.optimize.minimize(opti.fun, P0,
                               args=args,
                               method='L-BFGS-B', jac=opti.jac, bounds=None, tol=None, callback=None,
                               options={'disp': True, 'maxcor': 10, 'ftol': 1.e-09, 'gtol': 1e-03,
-                                       'eps': 1e-08, 'maxfun': 100, 'maxiter': 50, 'iprint': -1, 'maxls': 20})
+                                       'eps': 1e-08, 'maxfun': 100, 'maxiter': 5, 'iprint': -1, 'maxls': 20})
 #%%
 P1 = res['x']
 
@@ -173,11 +201,17 @@ opti.fill_Mod_from_Vector(P1, Mod_el_opti)
 #%%
 Modlist_opti_tot = shoot.shooting_traj(Mod_el_opti, N)
 
-
-
-
-
-
+#%% Visualisation
+xst_c = my_close(xst)
+xs_c = my_close(xs)
+for i in range(N + 1):
+    plt.figure()
+    xs_i = Modlist_opti_tot[2 * i].GD.GD_list[0].GD
+    xs_ic = my_close(xs_i)
+    plt.plot(xst_c[:, 0], xst_c[:, 1], '-k', linewidth=1)
+    plt.plot(xs_ic[:, 0], xs_ic[:, 1], '-g', linewidth=2)
+    plt.plot(xs_c[:, 0], xs_c[:, 1], '-b', linewidth=1)
+    plt.axis('equal')
 
 
 
