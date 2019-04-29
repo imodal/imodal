7fe554a909bfec3d30252a888c9f5bbd05917965
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2019-04-29 15:50:54 +0200

    fix visualization routines.

--- implicitmodules/torch/usefulfunctions.py
+++ implicitmodules/torch/usefulfunctions.py
@@ -8,6 +8,7 @@ from torchviz import make_dot
 
 class AABB:
     """Class used to represent an Axis Aligned Bounding Box"""
+
     def __init__(self, xmin=0., xmax=0., ymin=0., ymax=0.):
         self.__xmin = xmin
         self.__xmax = xmax
@@ -18,12 +19,12 @@ class AABB:
     def build_from_points(cls, points):
         """Compute the AABB from points"""
 
-        return cls(torch.min(points[:, 0]), torch.max(points[:, 0]),
-                    torch.min(points[:, 1]), torch.max(points[:, 1]))
-
+        return cls(points[:, 0].min(), points[:, 0].max(),
+                   points[:, 1].min(), points[:, 1].max())
+    
     def sample_random_point(self, count):
-        return torch.tensor([self.width, self.height])*torch.rand(count, 2)+torch.tensor([self.xmin, self.ymin])
-
+        return torch.tensor([self.width, self.height]) * torch.rand(count, 2) + torch.tensor([self.xmin, self.ymin])
+    
     def is_inside(self, points):
         return torch.where((points[:, 0] >= self.__xmin) & (points[:, 0] <= self.xmax) &
                            (points[:, 1] >= self.__ymin) & (points[:, 1] <= self.ymax),
@@ -62,13 +63,14 @@ class AABB:
 
     @property
     def area(self):
-        return (self.__xmax - self.__xmin)*(self.ymax - self.ymin)
-
+        return (self.__xmax - self.__xmin) * (self.ymax - self.ymin)
+    
     def squared(self):
+        enlarge = .1
         xmiddle = (self.__xmin + self.__xmax) / 2
         ymiddle = (self.__ymin + self.__ymax) / 2
     
-        diam = max(torch.abs(self.__xmin - self.__xmax) / 2, torch.abs(self.__ymin - self.__ymax) / 2)
+        diam = max(abs(self.__xmin - self.__xmax) / 2, abs(self.__ymin - self.__ymax) / 2) * (1 + enlarge)
         self.__xmin = xmiddle - diam
         self.__ymin = ymiddle - diam
         self.__xmax = xmiddle + diam
@@ -98,24 +100,27 @@ def vec2grid(vec, nx, ny):
 
 def indices2coords(indices, shape, pixel_size=torch.tensor([1., 1.])):
     # return torch.tensor([pixel_size[1]*indices[:, 1], pixel_size[0]*(shape[0] - indices[:, 0] - 1)])
-    return torch.cat([(pixel_size[0]*indices[:, 0]).view(-1, 1), (pixel_size[1]*(shape[1] - indices[:, 1] - 1)).view(-1, 1)], 1)
+    return torch.cat(
+        [(pixel_size[0] * indices[:, 0]).view(-1, 1), (pixel_size[1] * (shape[1] - indices[:, 1] - 1)).view(-1, 1)], 1)
 
 
 def blocks_to_2d(M):
     """Transforms a block matrix tensor (N x dim_block x dim_block) into a 2D square block matrix of size (N * dim_block x N * dim_block)."""
     N = int(math.sqrt(M.shape[0]))
-    assert N**2 == M.shape[0]
-    return torch.cat([torch.cat([M.transpose(1, 2)[i::N] for i in range(N)], dim=1).transpose(1, 2)[i] for i in range(N)])
+    assert N ** 2 == M.shape[0]
+    return torch.cat(
+        [torch.cat([M.transpose(1, 2)[i::N] for i in range(N)], dim=1).transpose(1, 2)[i] for i in range(N)])
 
 
 def blocks_to_2d_fast(M):
     a = torch.arange(M.numel()).view_as(M)
     indices = blocks_to_2d(a).view(-1)
-    return torch.take(M, indices).view(int(math.sqrt(M.shape[0])*M.shape[1]), int(math.sqrt(M.shape[0])*M.shape[2]))
+    return torch.take(M, indices).view(int(math.sqrt(M.shape[0]) * M.shape[1]), int(math.sqrt(M.shape[0]) * M.shape[2]))
 
 
 def kronecker(m1, m2):
-    return torch.ger(m1.view(-1), m2.view(-1)).reshape(*(m1.size() + m2.size())).permute([0, 2, 1, 3]).reshape(m1.size(0) * m2.size(0), m1.size(1) * m2.size(1))
+    return torch.ger(m1.view(-1), m2.view(-1)).reshape(*(m1.size() + m2.size())).permute([0, 2, 1, 3]).reshape(
+        m1.size(0) * m2.size(0), m1.size(1) * m2.size(1))
 
 
 def rot2d(theta):
@@ -129,21 +134,22 @@ def close_shape(x):
 
 def plot_tensor_scatter(x, alpha=1., scale=64.):
     """Scatter plot points in the format: ([x, y], scale) or ([x, y]) (in that case you can specify scale)"""
-    if(isinstance(x, tuple) or isinstance(x, list)):
-        plt.scatter(x[0].detach().numpy()[:,1], x[0].detach().numpy()[:,0], s=50.*x[1].shape[0]*x[1].detach().numpy(), marker='o', alpha=alpha)
-        plt.scatter(x[0].detach().numpy()[:,1], x[0].detach().numpy()[:,0], s=64.*x[1].shape[0]*x[1], marker='o', alpha=alpha)
+    if (isinstance(x, tuple) or isinstance(x, list)):
+        plt.scatter(x[0].detach().numpy()[:, 1], x[0].detach().numpy()[:, 0],
+                    s=50. * x[1].shape[0] * x[1].detach().numpy(), marker='o', alpha=alpha)
+        plt.scatter(x[0].detach().numpy()[:, 1], x[0].detach().numpy()[:, 0], s=64. * x[1].shape[0] * x[1], marker='o',
+                    alpha=alpha)
     else:
-        plt.scatter(x.detach().numpy()[:,1], x.detach().numpy()[:,0], s=scale, marker='o', alpha=alpha)
+        plt.scatter(x.detach().numpy()[:, 1], x.detach().numpy()[:, 0], s=scale, marker='o', alpha=alpha)
 
 
 def plot_grid(ax, gridx, gridy, **kwargs):
     for i in range(gridx.shape[0]):
-        ax.plot(gridx[i,:], gridy[i,:], **kwargs)
+        ax.plot(gridx[i, :], gridy[i, :], **kwargs)
     for i in range(gridx.shape[1]):
-        ax.plot(gridx[:,i], gridy[:,i], **kwargs)
+        ax.plot(gridx[:, i], gridy[:, i], **kwargs)
 
 
 def make_grad_graph(tensor, filename):
     dot = make_dot(tensor)
     dot.render(filename)
-
