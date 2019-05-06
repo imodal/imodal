09f6da278062dd501191640da1af300448c921f5
Gris Barbara <bgris.maths@gmail.com>
2019-05-06 17:49:05 +0200

    New class of Support for StructuredFields

--- implicitmodules/numpy/StructuredFields/StructuredField_p.py
+++ implicitmodules/numpy/StructuredFields/StructuredField_p.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import implicitmodules.numpy.StructuredFields.Abstract as ab
+import implicitmodules.numpy.StructuredFields.SupportPoints as supp
 from implicitmodules.numpy.Kernels import ScalarGaussian as ker
 
 
@@ -15,7 +16,7 @@ class StructuredField_p(ab.StructuredField):
         self.sig = sigma
         self.N_pts = N_pts
         self.type = 'p'
-        self.support = np.zeros([N_pts, dim])
+        self.support = supp.SupportPoints(N_pts, dim)
         self.mom = np.zeros([N_pts, dim, dim])
     
     def copy(self):
@@ -24,7 +25,7 @@ class StructuredField_p(ab.StructuredField):
     
     def copy_full(self):
         v = StructuredField_p(self.sig, self.N_pts, self.dim)
-        v.support = self.support.copy()
+        v.support = self.support.copy_full()
         v.mom = self.mom.copy()
         
         return v
@@ -34,7 +35,7 @@ class StructuredField_p(ab.StructuredField):
         param should be a list of two elements: array of points and 
         array of vectors
         """
-        self.support = param[0].copy()
+        self.support.fill_value(param[0])
         self.mom = param[1].copy()
     
     def Apply(self, z, j):
@@ -47,7 +48,7 @@ class StructuredField_p(ab.StructuredField):
         # lsize = ((Nz, 2), (Nz, 2, 2), (Nz, 2, 2, 2))
         # djv = np.zeros(lsize[j])
         
-        x = self.support.copy()
+        x = self.support.get_value()
         P = self.mom.copy()
         P = (P + np.swapaxes(P, 1, 2)) / 2
         ker_vec = -ker.my_vker(ker.my_xmy(z, x), j + 1, self.sig)
---
+++ implicitmodules/numpy/StructuredFields/SupportAbstract.py
@@ -0,0 +1,14 @@
+class SupportAbstract(object):
+    """
+    Abstract class for support of points, needs to have the 
+      following attributes:
+          
+          -- value : value of the support
+         
+          -- cotan : element of the cotangent 
+                     (ex: derivative of a scalar function wrt self)
+               
+    """
+    
+    def __init__(self):
+        pass
