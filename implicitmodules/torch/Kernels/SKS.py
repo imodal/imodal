5f338f2b97fe910a5f27cb90dfaa434fecf71949
Leander (Tensor) <leander.lacroix@ljll.math.upmc.fr>
2019-10-10 18:19:57 +0200

    Added 3D support to vec2grid and grid2vec + tests, cleanup of SKS.py and fit_3d.ipynb

--- implicitmodules/torch/Kernels/SKS.py
+++ implicitmodules/torch/Kernels/SKS.py
@@ -23,7 +23,7 @@ def eta(dim, device=None):
         eta[1, 2, 5] = cst
         eta[2, 1, 5] = cst
 
-        return eta
+        return eta.to(device=device)
     else:
         raise NotImplementedError
 
@@ -33,25 +33,14 @@ def compute_sks(x, sigma, order):
     dim = x.shape[1]
     sym_dim = int(dim * (dim + 1) / 2)
     N = x.shape[0]
-    if dim == 2:
-        if order == 0:
-            return torch.einsum('ij, kl->ijkl', gauss_kernel(rel_differences(x, x), 0, sigma).view(N, N), torch.eye(dim, device=device)).permute([0, 2, 1, 3]).contiguous().view(2 * N, 2 * N)
-        elif order == 1:
-            A = torch.tensordot(-gauss_kernel(rel_differences(x, x), 2, sigma), torch.eye(dim, device=device), dims=0)
-            K = torch.tensordot(torch.transpose(A, 2, 3), eta(dim, device=device))
-            K = torch.tensordot(K, eta(dim, device=device), dims=([1, 2], [0, 1]))
-            return K.view(N, N, 3, 3).contiguous().permute([0, 2, 1, 3]).contiguous().view(3 * N, 3 * N)
-        else:
-            raise NotImplementedError
-    elif dim == 3:
-        if order == 0:
-            return torch.einsum('ij, kl->ijkl', gauss_kernel(rel_differences(x, x), 0, sigma).view(N, N), torch.eye(dim, device=device)).permute([0, 2, 1, 3]).contiguous().view(dim * N, dim * N)
-        elif order == 1:
-            A = torch.tensordot(-gauss_kernel(rel_differences(x, x), 2, sigma), torch.eye(dim, device=device), dims=0)
-            K = torch.tensordot(torch.transpose(A, 2, 3), eta(dim, device=device))
-            K = torch.tensordot(K, eta(dim, device=device), dims=([1, 2], [0, 1]))
-            return K.view(N, N, sym_dim, sym_dim).contiguous().permute([0, 2, 1, 3]).contiguous().view(sym_dim * N, sym_dim * N)
-        else:
-            NotImplementedError
+
+    if order == 0:
+        return torch.einsum('ij, kl->ijkl', gauss_kernel(rel_differences(x, x), 0, sigma).view(N, N), torch.eye(dim, device=device)).permute([0, 2, 1, 3]).contiguous().view(dim * N, dim * N)
+    elif order == 1:
+        A = torch.tensordot(-gauss_kernel(rel_differences(x, x), 2, sigma), torch.eye(dim, device=device), dims=0)
+        K = torch.tensordot(torch.transpose(A, 2, 3), eta(dim, device=device))
+        K = torch.tensordot(K, eta(dim, device=device), dims=([1, 2], [0, 1]))
+        return K.view(N, N, sym_dim, sym_dim).contiguous().permute([0, 2, 1, 3]).contiguous().view(sym_dim * N, sym_dim * N)
     else:
-        raise NotImplementedError
+        NotImplementedError
+
