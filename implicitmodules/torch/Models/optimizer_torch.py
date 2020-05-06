Fixed bugs and added features to the new fitting interface. Added homemade gradient descent as well (still not 100% functional for some reason) The gradient descent seems not to work on all examples.

implicitmodules/torch/Models/optimizer_torch.py
@@ -5,26 +5,41 @@ from itertools import chain
 import torch
 
 from implicitmodules.torch.Models import BaseOptimizer, register_optimizer
-
+from implicitmodules.torch.Models.optimizer_gd import GradientDescentOptimizer
 
 
 class OptimizerTorch(BaseOptimizer):
     def __init__(self, torch_optimizer, model):
         self.__torch_optimizer = torch_optimizer
 
-        super().__init__(model, None)
+        # Creation of the Torch optimizer is deffered (optimization parameters are given
+        # when optimize() is called)
+        self.__optimizer = None
+
+        super().__init__(model)
+
+    @property
+    def method_name(self):
+        return self.__torch_optimizer.__module__.split(".")[0] + " " + self.__torch_optimizer.__name__
 
     def reset(self):
-        pass
+        self.__optimizer = None
 
-    def optimize(self, target, max_iter, post_iteration_callback, costs, shoot_solver, shoot_it, options=None):
+    def optimize(self, target, max_iter, post_iteration_callback, costs, shoot_solver, shoot_it, tol, options=None):
         if options is None:
             options = {}
+
+        if tol is None:
+            tol = 1e-10
+
+        # If the optimizer has not been created before, create it now
+        if self.__optimizer is None:
+            self.__optimizer = self.__torch_optimizer(list(self.model.parameters.values()), **options)
 
         self.__eval_count = 0
         self.__last_costs = None
         def _evaluate():
-            self.__torch_optimizer.zero_grad()
+            self.__optimizer.zero_grad()
 
             costs = self.model.evaluate(target, shoot_solver, shoot_it)
 
@@ -32,16 +47,12 @@ class OptimizerTorch(BaseOptimizer):
             self.__eval_count = self.__eval_count + 1
             return costs['total']
 
-        ftol = 1e-5
-        if 'ftol' in options:
-            ftol = options['ftol']
-
         cont = True
         termination_state = 0
         it = 0
         last_total = float('inf')
         for i in range(max_iter):
-            self.__torch_optimizer.step(_evaluate)
+            self.__optimizer.step(closure=_evaluate)
 
             post_iteration_callback(self.model, self.__last_costs)
 
@@ -50,7 +61,7 @@ class OptimizerTorch(BaseOptimizer):
             if math.isnan(self.__last_costs['total']):
                 return {'success': False, 'final': float('nan'), 'message': "Evaluated function gave NaN.", 'neval': self.__eval_count}
 
-            if (self.__last_costs['total'] - last_total)/max(self.__last_costs['total'], last_total, 1) <= ftol:
+            if (last_total - self.__last_costs['total'])/max(self.__last_costs['total'], last_total, 1) <= tol:
                 return {'success': True, 'final': self.__last_costs['total'], 'message': "Convergence achieved.", 'neval': self.__eval_count}
 
             last_total = self.__last_costs['total']
@@ -58,20 +69,14 @@ class OptimizerTorch(BaseOptimizer):
         return {'success': False, 'final': self.__last_costs['total'], 'message': "Total number of iterations reached.", 'neval': self.__eval_count}
 
 
-def __parameters_to_list(parameters):
-    if isinstance(parameters, dict):
-        return list(chain(*parameters.values()))
-
-    return list(parameters)
-
-
 def __create_torch_optimizer(torch_optimizer):
-    def _create(model, **kwargs):
-        return OptimizerTorch(torch_optimizer(__parameters_to_list(model.parameters), **kwargs), model)
+    def _create(model):
+        return OptimizerTorch(torch_optimizer, model)
 
     return _create
 
 register_optimizer("torch_sgd", __create_torch_optimizer(torch.optim.SGD))
 register_optimizer("torch_lbfgs", __create_torch_optimizer(torch.optim.LBFGS))
 
+register_optimizer("gd", __create_torch_optimizer(GradientDescentOptimizer))
 