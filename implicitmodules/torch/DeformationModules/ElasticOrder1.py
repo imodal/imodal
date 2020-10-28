Fixed bug in manifold gradient.

implicitmodules/torch/DeformationModules/ElasticOrder1.py
@@ -1,4 +1,5 @@
 import torch
+import pickle
 
 from pykeops.torch import Genred, KernelSolve
 
@@ -215,7 +216,9 @@ class ImplicitModule1_KeOps(ImplicitModule1Base):
         S = 0.5 * (d_vx + torch.transpose(d_vx, 1, 2))
         S = torch.tensordot(S, eta(self.manifold.dim, device=self.device), dims=2)
 
+        print("tlambdas...")
         tlambdas = self.solve_sks(self.manifold.gd[0].reshape(-1, self.dim), self.manifold.gd[0].reshape(-1, self.dim), self.coeff * S, self.__keops_eye, self.__keops_invsigmasq, self.__keops_A, backend=self.__keops_backend, alpha=self.nu)
+        print("Done.")
 
         (aq, aqkiaq) = self.__compute_aqkiaq()
 
@@ -234,7 +237,9 @@ class ImplicitModule1_KeOps(ImplicitModule1Base):
 
     def __compute_moments(self):
         self.__aqh = self.__compute_aqh(self.controls)
+        print("Solve lambdas...")
         self.__lambdas = self.solve_sks(self.manifold.gd[0].reshape(-1, self.dim), self.manifold.gd[0].reshape(-1, self.dim), self.__aqh, self.__keops_eye, self.__keops_invsigmasq, self.__keops_A, backend=self.__keops_backend, alpha=self.nu)
+        print("End")
         self.moments = torch.tensordot(self.__lambdas.view(-1, self.sym_dim), torch.transpose(eta(self.manifold.dim, device=self.device), 0, 2), dims=1)
 
     def __compute_aqkiaq(self):
@@ -246,7 +251,7 @@ class ImplicitModule1_KeOps(ImplicitModule1Base):
             aqi = self.__compute_aqh(h).flatten()
             aq[:, i] = aqi
 
-            lambdas[i, :] = self.solve_sks(self.manifold.gd[0], self.manifold.gd[0], aqi.view(-1, self.sym_dim), self.__keops_eye, self.__keops_invsigmasq, self.__keops_A, backend=self.__keops_backend, alpha=self.nu).view(-1)
+            lambdas[i, :] = self.solve_sks(self.manifold.gd[0], self.manifold.gd[0], aqi.view(-1, self.sym_dim), self.__keops_eye, self.__keops_invsigmasq, self.__keops_A, backend=self.__keops_backend, alpha=self.nu, eps=1e-3).view(-1)
 
         return (aq, torch.mm(lambdas, aq))
 

implicitmodules/torch/Models/deformable.py
@@ -131,6 +131,7 @@ class DeformablePoints(Deformable):
     def _to_deformed(self, gd):
         return (gd,)
 
+
 # class DeformablePolylines(DeformablePoints):
 #     def __init__(self, points, connections):
 #         self.__connections = connections
@@ -153,7 +154,7 @@ class DeformableMesh(DeformablePoints):
     @classmethod
     def load_from_file(cls, filename, dtype=None):
         mesh = meshio.read(filename)
-        points = torch.tensor(mesh.points)
+        points = torch.tensor(mesh.points, dtype=dtype)
         triangles = torch.tensor(mesh.cell_dict['triangle'], torch.int)
         return cls(points, triangles)
 
@@ -183,7 +184,7 @@ class DeformableImage(Deformable):
         self.__pixel_extent = AABB(0., self.__shape[1]-1, 0., self.__shape[0]-1)
 
         if extent is None:
-            extent = AABB(0, 1., 0., 1.)
+            extent = AABB(0., 1., 0., 1.)
         elif isinstance(extent, str) and extent == 'match':
             extent = self.__pixel_extent
 
@@ -240,7 +241,6 @@ class DeformableImage(Deformable):
     output = property(__set_output, __get_output)
 
     def _backward_module(self):
-        # pixel_grid = pixels2points(self.__extent.fill_count(self.__shape), self.__shape, self.__extent)
         pixel_grid = pixels2points(self.__pixel_extent.fill_count(self.__shape), self.__shape, self.__extent)
         return SilentLandmarks(2, pixel_grid.shape[0], gd=pixel_grid)
 
@@ -296,15 +296,13 @@ def deformables_compute_deformed(deformables, modules, solver, it, costs=None, i
     shoot_backward = any([deformable._has_backward for deformable in deformables])
 
     forward_silent_modules = copy.deepcopy(silent_modules)
-    #[silent_module.manifold.requires_grad_(False) for silent_module in silent_modules]
+    # forward_silent_modules = silent_modules
 
-    # if backward_silent_modules is not None:
     if shoot_backward:
         # Backward shooting is needed
 
         # Build/assemble the modules that will be shot backward
         backward_modules = [deformable._backward_module() for deformable in deformables if deformable._has_backward]
-        # compound = CompoundModule([*backward_silent_modules, *backward_modules, *modules])
         compound = CompoundModule([*silent_modules, *backward_modules, *modules])
 
         # Reverse the moments for backward shooting
@@ -319,12 +317,6 @@ def deformables_compute_deformed(deformables, modules, solver, it, costs=None, i
 
     # Ugly way to compute the list of deformed objects. Not intended to stay!
     deformed = []
-    # for deformable in deformables:
-    #     if deformable._has_backward:
-    #         deformed.append(deformable._to_deformed(backward_modules.pop(0).manifold.gd))
-    #     else:
-    #         deformed.append(deformable._to_deformed(deformable.silent_module.manifold.gd))
-
     for deformable, forward_silent_module in zip(deformables, forward_silent_modules):
         if deformable._has_backward:
             deformed.append(deformable._to_deformed(backward_modules.pop(0).manifold.gd))